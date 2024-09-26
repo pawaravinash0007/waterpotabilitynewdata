edb2ab10713a71156c17a7a23943ab8b1611398b
Avinash Pawar <41367003+pawaravinash0007@users.noreply.github.com>
2024-09-26 15:07:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ import pandas as pd
 import numpy as np
 import pickle
 
+clf = pickle.load(open("water.pkl","rb"))
 
-
-def predict(data):
-    clf = pickle.load(open("water.pkl","rb"))
+def predict(data):    
     return clf.predict(data)
 
 
