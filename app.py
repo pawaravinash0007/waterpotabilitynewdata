04b74a2535db9af1efd4b0a0ef1e461e3fc787e4
Avinash Pawar <41367003+pawaravinash0007@users.noreply.github.com>
2024-09-26 16:55:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
+import pandas as pd
 import joblib
-
 # Load the model once at the start
-with open("water.pkl", "rb") as model_file:
-    clf = joblib.load(model_file)
+grid_search = joblib.load(open("water.pkl", "rb"))
 
+def predict(data):
+    grid_search = joblib.load(open("water.pkl", "rb"))
+    return grid_search.predict(data)
 #def predict(data):
     #return clf.predict(data)
 
@@ -30,15 +31,11 @@ with col2:
     Tu = st.slider("Turbidity Value", 1.0, 7.0, 2.0)  # Default value
     ii = st.slider("ID", 1.0, 3280.0, 1.0)  # Default value
 
-if st.button("Predict Potability"):
-    # Prepare input data for prediction
-    #input_data = np.array([pp, Hd, So, Ch, Co, Oc, Tr, Tu, ii])
-    result = clf.predict(np.array[pp, Hd, So, Ch, Co, Oc, Tr, Tu, ii])
+if st.button("Predict Survival"):
+    result = grid_search.predict(
+        np.array([[pp, Hd, So, Ch, Su,Co,Oc, Tr, Tu, ii]]))
+    st.text(result[0])
+    
 
-    # Display the result
-    if result[0] == 1:
-        st.success("The water is predicted to be potable.")
-    else:
-        st.error("The water is predicted not to be potable.")
 
 st.markdown("Developed at IIIT Surat")
