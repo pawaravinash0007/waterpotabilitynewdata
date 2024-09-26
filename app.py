f2946ae4ade737bfb5a0d863a22f66f3c51422f1
Avinash Pawar <41367003+pawaravinash0007@users.noreply.github.com>
2024-09-26 15:08:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ col1,col2 = st.columns(2)
 
 with col1:
 	
-	p = st.slider("Ph Value", 1.0, 14.0, 0.5)
+	p1 = st.slider("Ph Value", 1.0, 14.0, 0.5)
 	Hd = st.slider("Hardness Value", 65.0, 330.0, 0.5)
 	So = st.slider("Solids Value", 600.0,62000.0,0.5)
 	Ch = st.slider("Chloramines Value", 0.0, 14.0, 0.5)
@@ -26,10 +26,10 @@ with col1:
 	Oc = st.slider("Organic Carbon Value", 2.0, 30.0, 0.5)
 	Tr = st.slider("Trihalomethanes Value", 5.0, 130.0, 0.5)
 	Tu = st.slider("Turbidity Value", 1.0, 7.0, 0.5)	
-	i = st.slider("id", 1.0, 3280.0, 0.5)
+	i1 = st.slider("id", 1.0, 3280.0, 0.5)
 st.text('')
 if st.button("Seles Prediction "):
-    result = clf.predict(np.array([[p.Hd,So,Ch,Co,Oc,Tr,Tu,i]]))
+    result = clf.predict(np.array([[p1,Hd,So,Ch,Co,Oc,Tr,Tu,i1]]))
     st.text(result[0])
 
 st.markdown("Work at IIIT Surat")
